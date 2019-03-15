59f1d2d52af88d0e61c3c48c88b4769636163ee3
Kastuś <mitrat2@mail.ru>
2019-03-15 11:32:35 +0300

    Create GlobalParams.py

---
+++ GlobalParams.py
@@ -0,0 +1,7 @@
+CURR_OS = 'lin'
+SLASH_OS = '/'
+PASSWORD_PSQL = 'password'
+HOST_PSQL = 'localhost'
+USERNAME_PSQL = 'postgres'
+ROOT_DIR = "c:/BACKUP/posql"
+FORMAT_NAME = "{year}{month}{day}_{hour}{minute}"
